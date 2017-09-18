f7572d56ad570962e8f3c32e3e1b355ded783af9
Tristan Seifert <t@tseifert.me>
2017-09-17 20:34:31 -0500

    Initial Commit

---
+++ Lichtenstein/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lichtenstein
+//
+//  Created by Tristan Seifert on 2017-09-17.
+//  Copyright © 2017 Tristan Seifert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lichtenstein/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lichtenstein
+//
+//  Created by Tristan Seifert on 2017-09-17.
+//  Copyright © 2017 Tristan Seifert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
